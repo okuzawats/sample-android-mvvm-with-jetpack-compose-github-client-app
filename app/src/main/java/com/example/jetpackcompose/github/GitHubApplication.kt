489a40fd071d0dabe33bd10c9b427aa858e98a36
okuzawats <okuzawats@gmail.com>
2021-05-14 15:13:31 +0900

    add KDoc to GitHubApplication

--- app/src/main/java/com/example/jetpackcompose/github/GitHubApplication.kt
+++ app/src/main/java/com/example/jetpackcompose/github/GitHubApplication.kt
@@ -3,5 +3,10 @@ package com.example.jetpackcompose.github
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * 独自のApplicationクラス
+ *
+ * Hiltの @HiltAndroidApp を付与するために作成
+ */
 @HiltAndroidApp
 class GitHubApplication : Application()
