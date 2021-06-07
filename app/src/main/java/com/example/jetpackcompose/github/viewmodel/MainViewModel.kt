2552434dc097cd61d5c25b51c6e3a61e091e67b8
okuzawats <okuzawats@gmail.com>
2021-06-07 13:13:52 +0900

    add KDoc to MainViewModel

--- app/src/main/java/com/example/jetpackcompose/github/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/jetpackcompose/github/viewmodel/MainViewModel.kt
@@ -18,17 +18,48 @@ class MainViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
 
+    /**
+     * Viewの状態を表すsealed class
+     */
     sealed class UiState {
+
+        /**
+         * 初期状態
+         */
         object Initial : UiState()
+
+        /**
+         * 読み込み中
+         */
         object Loading : UiState()
+
+        /**
+         * 読み込み成功
+         */
         data class Success(val user: User) : UiState()
+
+        /**
+         * 読み込み失敗
+         */
         object Failure : UiState()
     }
 
+    /**
+     * Viewの状態を[UiState]として表すMutableState
+     */
     val uiState: MutableState<UiState> = mutableStateOf(UiState.Initial)
 
+    /**
+     * 検索フォームに入力された文字列を表すMutableState
+     */
     val searchQuery: MutableState<String> = mutableStateOf("")
 
+    /**
+     * 検索を実行する。
+     *
+     * searchQueryから検索フォームに入力された文字列を取得し、
+     * Repositoryを経由してユーザを問い合わせる。
+     */
     fun onSearchTapped() {
         val searchQuery: String = searchQuery.value
 
